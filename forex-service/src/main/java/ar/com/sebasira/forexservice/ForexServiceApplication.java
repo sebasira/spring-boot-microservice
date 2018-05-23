392c9303761e9ada058bc7dbfb7abfd1c7bd2068
Sebastian M. Irazabal <sebasira@gmail.com>
2018-05-23 01:40:09 -0300

    Services connected via Eureka

--- forex-service/src/main/java/ar/com/sebasira/forexservice/ForexServiceApplication.java
+++ forex-service/src/main/java/ar/com/sebasira/forexservice/ForexServiceApplication.java
@@ -2,8 +2,11 @@ package ar.com.sebasira.forexservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ForexServiceApplication {
 
 	public static void main(String[] args) {

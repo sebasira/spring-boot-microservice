075f8fb64cdf57815fa078ad383f72d8ce18823a
Sebastian M. Irazabal <sebasira@gmail.com>
2018-05-23 00:37:12 -0300

    Added Eureka Naming Server

---
+++ eureka-naming-server/src/main/java/ar/com/sebasira/eurekanamingserver/EurekaNamingServerApplication.java
@@ -0,0 +1,14 @@
+package ar.com.sebasira.eurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
+	}
+}
